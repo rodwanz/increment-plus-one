11330b48efaaa3355ba748cd7d4fe11da0ca64f4
rodwanz <wanzervai@gmail.com>
2026-03-30 17:47:22 -0300

    System running

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "https://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [ring/ring-core "1.10.0"]
+                 [ring/ring-jetty-adapter "1.10.0"]
+                 [compojure "1.7.0"]]
   :repl-options {:init-ns increment-plus-one.core}
   :main increment-plus-one.core)
--- src/increment_plus_one/core.clj
+++ src/increment_plus_one/core.clj
@@ -1,12 +1,19 @@
 (ns increment-plus-one.core
+  (:require [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]
+            [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
 (defn increment [n]
   (inc n))
 
+(defroutes app-routes
+           (GET "/inc/:value" [value]
+                (let [number (Integer/parseInt value)]
+                  (str "Result: " (increment number))))
+           (route/not-found "Route not found"))
+
 (defn -main [& args]
-  (println "Enter a number")
-  (let [input (read-line)
-        number (Integer/parseInt input)]
-    (println "Result: " (increment number))))
+  (println "Server running on http://localhost:3000")
+  (run-jetty app-routes {:port 3000 :join? false }))
 
